0c50a226e09f16f56ba89a3247ca91f0d27d9536
37sy <SqSouYou@aliyun.com>
2021-07-16 17:20:35 +0800

    例子：每次执行Activity的startActivity方法时,都打印一行日志

---
+++ hookfixstudy/src/main/java/com/sq/mobile/hookfixstudy/EvilInstrumentation.java
@@ -0,0 +1,60 @@
+package com.sq.mobile.hookfixstudy;
+
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+
+/***
+ *
+ * 静态代理的模式
+ *
+ * */
+public class EvilInstrumentation extends Instrumentation {
+
+    private static final String TAG = "EvilInstrumentation";
+
+    // ActivityThread中原始的对象, 保存起来
+    Instrumentation mBase;
+
+    public EvilInstrumentation(Instrumentation base) {
+        mBase = base;
+    }
+
+    public ActivityResult execStartActivity(
+            Context who, IBinder contextThread, IBinder token, Activity target,
+            Intent intent, int requestCode, Bundle options) {
+
+        Log.d(TAG, "XXX到此一游!");
+
+        // 开始调用原始的方法, 调不调用随你,但是不调用的话, 所有的startActivity都失效了.
+        // 由于这个方法是隐藏的,因此需要使用反射调用;首先找到这个方法
+        Class[] p1 = {
+                Context.class,
+                IBinder.class,
+                IBinder.class,
+                Activity.class,
+                Intent.class,
+                int.class,
+                Bundle.class
+        };
+        Object[] v1 = {
+                who,
+                contextThread,
+                token,
+                target,
+                intent,
+                requestCode,
+                options};
+        return (ActivityResult) RefInvoke.invokeInstanceMethod(
+                mBase,
+                "execStartActivity",
+                p1,
+                v1);
+    }
+
+}
